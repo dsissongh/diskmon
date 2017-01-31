59c7f55adea08e2110986dd6388c7e11d7f5d01e
David Sisson <davidsissonemail@gmail.com>
2017-01-30 20:59:51 -0800

    Added optional positional arguments - determine report indicated

--- diskmon/main.py
+++ diskmon/main.py
@@ -1,4 +1,6 @@
+import sys
 import psutil
+import argparse
 from prettytable import PrettyTable
 
 
@@ -14,10 +16,35 @@ def getdiskinfo():
 
 	return disks
 
+
+parser = argparse.ArgumentParser()
+parser.add_argument('--report', '-r', help='In additon to visual report, send an email when the value is lower than specified.',
+			action="store_true")
+parser.add_argument('--limit', '-l', help='Set the limit in Gb.  Default = 4G',
+			nargs='?', const=4, type=int)
+parser.parse_args()
+args = parser.parse_args()
+
+
 table = PrettyTable(['Drive','Freespace'])
 drives = getdiskinfo()
+report = []
 for drive in drives:
 	table.add_row([drive,drives[drive]])
-	
+	if args.report:
+		if args.limit:
+			limit = args.limit
+		else:
+			limit = 4
+
+		if drives[drive] < limit:	
+			report.append(drive)
+
+
+
 table.sortby =  'Freespace'	
 print(table)
+
+if args.report:
+	if len(report) > 0:
+		print(report)
